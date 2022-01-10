a663d08aceb10d04a88effa1c98ca5f6b96cfd81
Sergio Rosa <scarlosjazz@gmail.com>
2022-01-10 11:45:19 -0500

    Navbar adding baseUrl: ./src

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,9 @@
-import {ReactComponent as GithubIcon } from './assets/img/github.svg'
-import './index.css';
+import Navbar from "components/Navbar";
+
 
 function App () {
-  return (
-    <header>
-      <nav className='container'>
-        <div className='dsmovie-nav-content'>
-          <h1>MovieApp</h1>
-          <a
-            href='https://github.com/srosajazz'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <div className='dsmovie-contact-container'>
-              <GithubIcon />
-              <p className='dsmovie-contact-link'>/sergiorosa</p>
-            </div>
-          </a>
-        </div>
-      </nav>
-    </header>
+  return(
+  <Navbar/>
   )
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,26 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
+
+
+function Navbar() {
+  return (
+    <header>
+      <nav className='container'>
+        <div className='dsmovie-nav-content'>
+          <h1>MovieApp</h1>
+          <a
+            href='https://github.com/srosajazz'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <div className='dsmovie-contact-container'>
+              <GithubIcon />
+              <p className='dsmovie-contact-link'>/sergiorosa</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar;
