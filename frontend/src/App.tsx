de48810c409be35838e0be2aab420c69714fb5c0
Sergio Rosa <scarlosjazz@gmail.com>
2022-01-10 11:32:25 -0500

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
-
+import {ReactComponent as GithubIcon } from './assets/img/github.svg'
+import './index.css';
 
 function App () {
   return (
-   <h1>Page One</h1>
+    <header>
+      <nav className='container'>
+        <div className='dsmovie-nav-content'>
+          <h1>MovieApp</h1>
+          <a
+            href='https://github.com/srosajazz'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <div className='dsmovie-contact-container'>
+              <GithubIcon />
+              <p className='dsmovie-contact-link'>/sergiorosa</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
   )
 }
 
